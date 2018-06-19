5bfe9110136a05c3d8579b8e55df7ce2774730a2
Hunter Sapienza <sapienzahunter@gmail.com>
2018-06-19 15:40:41 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,54 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    song = self.new
+    song.name = name
+    if self.find_by_name(name)
+      self.find_by_name(name) # why is this line more complicated than seems like it should be?
+    else self.create_by_name(name)
+    end
+  end
+  
+  def self.alphabetical
+    sorted = @@all.sort_by{|song| song.name}
+   sorted
+  end
+  
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    
+    name = parts[1].chomp(".mp3")
+    artist_name = parts[0]
+    
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    song
+  end
+
 end
